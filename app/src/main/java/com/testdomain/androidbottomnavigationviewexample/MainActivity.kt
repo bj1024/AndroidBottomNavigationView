c571c33c143b0d6351398894b5327b4f20b8e501
H.K <16552133+bj1024@users.noreply.github.com>
2020-08-13 19:15:54 +0900

    bottom navigation bar shitmode...Pending. not worked.

--- app/src/main/java/com/testdomain/androidbottomnavigationviewexample/MainActivity.kt
+++ app/src/main/java/com/testdomain/androidbottomnavigationviewexample/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.testdomain.androidbottomnavigationviewexample
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomnavigation.LabelVisibilityMode
+
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
@@ -39,9 +43,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+//        navView.disableShiftMode()
+        BottomNavigationHelper.removeShiftMode(bottomNavigationView)
+
+        // force showing title
+        val menuView: BottomNavigationMenuView = bottomNavigationView.getChildAt(0) as BottomNavigationMenuView
+        for (i in 0..menuView.childCount - 1) {
+            val itemView = menuView.getChildAt(i) as BottomNavigationItemView
+            itemView.setShifting(false)
+//            itemView.setChecked(false)
+            itemView.setChecked(itemView.itemData.isChecked)
+        }
+
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
@@ -55,5 +71,45 @@ class MainActivity : AppCompatActivity() {
 //        navView.setupWithNavController(navController)
 
 
+    }
+
+
+
+}
+
+//
+//@SuppressLint("RestrictedApi")
+//fun BottomNavigationView.disableShiftMode() {
+//    val menuView = getChildAt(0) as BottomNavigationMenuView
+//    try {
+//        val shiftingMode = menuView::class.java.getDeclaredField("mShiftingMode")
+//        shiftingMode.isAccessible = true
+//        shiftingMode.setBoolean(menuView, false)
+//        shiftingMode.isAccessible = false
+//        for (i in 0 until menuView.childCount) {
+//            val item = menuView.getChildAt(i) as BottomNavigationItemView
+//            item.setShifting(false)
+//            // set once again checked value, so view will be updated
+//            item.setChecked(item.itemData.isChecked)
+//        }
+//    } catch (e: NoSuchFieldException) {
+//        Log.e("BottomNavigationView", "Unable to get shift mode field", e)
+//    } catch (e: IllegalStateException) {
+//        Log.e("BottomNavigationView", "Unable to change value of shift mode", e)
+//    }
+//}
+
+private object BottomNavigationHelper {
+    @SuppressLint("RestrictedApi")
+    fun removeShiftMode(view: BottomNavigationView) {
+        val menuView = view.getChildAt(0) as BottomNavigationMenuView
+        for (i in 0 until menuView.childCount) {
+            val item = menuView.getChildAt(i) as BottomNavigationItemView
+            item.setShifting(false)
+            item.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED)
+
+            // set once again checked value, so view will be updated
+            item.setChecked(item.itemData.isChecked)
+        }
     }
 }
